081fad8d6774dc0cef48a2221fe074a65e29ebb0
sjjh <simon.harhues@muenster.de>
2013-01-29 20:44:01 +0100

    JavaBeans Category (Enum) and ToDoItem added

---
+++ devguiGroup8/src/model/ToDoItem.java
@@ -0,0 +1,134 @@
+package model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.EnumSet;
+
+/**
+ * JavaBean that represents a ToDo-list item.
+ * @author simon
+ *
+ */
+@SuppressWarnings("serial") //TODO: we should make sure within the model, that not
+							//		two items with the same content are created!
+public class ToDoItem implements Serializable {
+
+	private String	title;
+	private String	description;
+	private Date	dueDate;
+	private int		priority;
+	private EnumSet<Category>	categories;
+	private boolean	done;
+	private boolean	deleted;
+	private Date	creationDate;
+	
+	
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+	/**
+	 * @param title the title of the ToDo-item to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description of the ToDo-item to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	/**
+	 * @return the dueDate
+	 */
+	public Date getDueDate() {
+		return dueDate;
+	}
+	/**
+	 * @param dueDate the dueDate to set
+	 */
+	public void setDueDate(Date dueDate) {
+		this.dueDate = dueDate;
+	}
+	/**
+	 * @return the priority
+	 */
+	public int getPriority() {
+		return priority;
+	}
+	/**
+	 * TODO: what about the range, e.g. 0-5? And how (silent set within border,
+	 * 			throw exception, ...) and where (here, in the model) to control?
+	 * @param priority the priority to set
+	 */
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+	/**
+	 * @return the List of all categories
+	 */
+	public EnumSet<Category> getCategories() {
+		return categories;
+	}
+	/**
+	 * @param categories the list of all categories to set
+	 */
+	public void setCategories(EnumSet<Category> categories) {
+		this.categories = categories;
+	}
+	/**
+	 * @param category the category to add to the list of categories
+	 */
+	public void addCategory(Category category) {
+		this.categories.add(category);
+	}
+	/**
+	 * @return the status if item is done
+	 */
+	public boolean isDone() {
+		return done;
+	}
+	/**
+	 * @param done the status if item is done to set
+	 */
+	public void setDone(boolean done) {
+		this.done = done;
+	}
+	/**
+	 * @return the status if item is deleted
+	 */
+	public boolean isDeleted() {
+		return deleted;
+	}
+	/**
+	 * @param deleted the status if item is deleted to set
+	 */
+	public void setDeleted(boolean deleted) {
+		this.deleted = deleted;
+	}
+	/**
+	 * @return the creationDate
+	 */
+	public Date getCreationDate() {
+		return creationDate;
+	}
+	/**
+	 * TODO: normally we woudln't need this method, as we can set it in the 
+	 * 			constructor, but as this class is serializeable we might need it?
+	 * 			How to assure, that the creation date is not altered later on?
+	 * @param creationDate the creationDate to set
+	 */
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+	
+}
