7399d3016e218e0e63065fd6dc358eaf456501e5
emra <emma.rangert@gmail.com>
2013-02-05 18:36:13 +0100

    Updated my main view

--- src/view/MainView.java
+++ src/view/MainView.java
@@ -1,20 +1,17 @@
 import java.awt.BorderLayout;
 import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.ScrollPane;
 
-import javax.swing.BorderFactory;
-import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JList;
+import javax.swing.JLabel;
+import javax.swing.BorderFactory;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
-import javax.swing.ListSelectionModel;
 
 /**
  * 
@@ -22,69 +19,100 @@ import javax.swing.ListSelectionModel;
  *
  */
 
-public class MainView {
-	public static void addComponentsToPane(Container pane) {
-        
-		JList list = new JList(new DefaultListModel());//listModel);
-		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		list.setSelectedIndex(0);
-		list.setVisibleRowCount(10);
-		JScrollPane scrollPane = new JScrollPane(list);
-		pane.add(scrollPane,BorderLayout.PAGE_START);
-		
-		JPanel mainPanel = new JPanel(new BorderLayout());
-	    pane.add(mainPanel);
-		
-	    
-	    JPanel textFieldPanel = new JPanel(new FlowLayout());
-	    JTextField textField = new JTextField();
-	    textField.setPreferredSize(new Dimension(300,20));//pane.getWidth(),20)); //Hard coded for now...
-	    textFieldPanel.add(textField);
-	    
-	    JPanel buttonFieldPanel = new JPanel(new FlowLayout());
-	    JButton addButton = new JButton("+");
-	    addButton.setPreferredSize(new Dimension(20,20));
-	    buttonFieldPanel.add(addButton);
-	    
-	    
-	    /*
-	    JTextField textField = new JTextField(new FlowLayout().LEFT);
-	    textField.setPreferredSize(new Dimension(300,20));
-	    mainPanel.add(textField);
-	    
-	    JButton addButton = new JButton("+");
-	    addButton.setPreferredSize(new Dimension(20,20));
-	    mainPanel.add(addButton);
-	    */
-	    //mainPanel.setBorder(BorderFactory.createEmptyBorder(0,10,0,0));
+@SuppressWarnings("serial")
 
-	    mainPanel.add(textFieldPanel,BorderLayout.SOUTH);
-	    mainPanel.add(buttonFieldPanel, BorderLayout.LINE_END);
-    }
-
-	private static void createAndShowGUI() {
-        
-        JFrame frame = new JFrame("The Greight TODO Manager");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(350, 500);
-        addComponentsToPane(frame.getContentPane());
-        
-        JMenuBar menuBar = new JMenuBar();
+public class MainView extends JFrame {
+	
+	private static int windowHeight = 600;
+	private static int windowWidth = 500;
+	
+	private static void addMenuBar(JFrame frame) {
+		
+		JMenuBar menuBar = new JMenuBar();
 		frame.setJMenuBar(menuBar);
-		
+
 		JMenu file = new JMenu("File");
 		menuBar.add(file);
 		JMenu edit = new JMenu("Edit");
 		menuBar.add(edit);
 		JMenu help = new JMenu("Help");
 		menuBar.add(help);
-        
+		
+	}
+	
+	private static void addComponentsToPane(Container pane) {
+		
+		
+		// Labels
+		JLabel item = new JLabel("Item");
+		JLabel category = new JLabel("Category");
+		JLabel priority = new JLabel("Priority");
+	    JLabel deadline = new JLabel("Deadline");
+		
+		// Table
+		JTable table = new JTable();
+		
+		// Scroll pane
+	    ScrollPane scrollPane = new ScrollPane();
+
+	    // Panels
+	    JPanel northPanel = new JPanel();
+	    JPanel southPanel = new JPanel();
+	    
+	    // Buttons
+	    JButton addBtn = new JButton("Add");
+	   
+	    // Text fields
+	    JTextField inputFld = new JTextField();
+	    
+	    pane.setLayout(new BorderLayout());
+	    scrollPane.add(table);
+	    
+	    // Add to pane
+	    pane.add(scrollPane, BorderLayout.CENTER);
+		pane.add(northPanel, BorderLayout.NORTH);
+	    pane.add(southPanel, BorderLayout.SOUTH);
+	    
+	    // Set layouts
+	    southPanel.setLayout(new BorderLayout());
+	    northPanel.setLayout(new GridLayout());
+	    
+	    // Setting background color for help
+        //northPanel.setBackground(Color.ORANGE);
+        //southPanel.setBackground(Color.GREEN);
+        //scrollPane.setBackground(Color.YELLOW);
+	    
+	    inputFld.setHorizontalAlignment(JTextField.LEFT);
+
+	    // Adding to southPanel
+	    southPanel.add(inputFld, BorderLayout.CENTER);
+	    southPanel.add(addBtn, BorderLayout.EAST);
+        southPanel.setBorder(BorderFactory.createTitledBorder("Add to do"));
+
+	    // Adding to northPanel
+	    northPanel.add(item);
+	    northPanel.add(category);
+	    northPanel.add(priority);
+	    northPanel.add(deadline);
+	    northPanel.setBorder(BorderFactory.createTitledBorder("To do's"));
+	    
+	    table.setBorder(BorderFactory.createTitledBorder(""));	    
+	}
+
+	private static void createAndShowGUI() {
+		
+		JFrame frame = new JFrame("The Greight TODO Manager");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(windowWidth, windowHeight);
+		
+		addComponentsToPane(frame.getContentPane());
+		addMenuBar(frame);
+		
+		frame.setVisible(true);
 		frame.pack();
-        frame.setVisible(true);
     }
 	
 	public static void main(String[] args) {
-
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 createAndShowGUI();
