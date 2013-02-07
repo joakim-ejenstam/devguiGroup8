52a733de315dc367f110c2f065e72301f0652a91
Joakim Ejenstam <joakim.ejenstam@gmail.com>
2013-02-07 10:44:49 +0100

    Started working on the application bootstrap and an simple stupid controller.

---
+++ src/ApplicationStartup.java
@@ -0,0 +1,18 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Joakim
+ * Date: 2013-02-07
+ * Time: 09:50
+ * To change this template use File | Settings | File Templates.
+ */
+public class ApplicationStartup {
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                view.MainView.createAndShowGui();
+            }
+        });
+
+    }
+}
