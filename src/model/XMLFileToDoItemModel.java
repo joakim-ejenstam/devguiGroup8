04b1caae2cd4f7ca31c67f956f334bb6ee49d862
Matteh <Matteh@123>
2013-02-16 14:59:36 +0100

    Added functionality to all methods, added some of the duplicate code inside new private methods..

--- src/model/XMLFileToDoItemModel.java
+++ src/model/XMLFileToDoItemModel.java
@@ -1,16 +1,21 @@
 package model;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import nu.xom.Builder;
 import nu.xom.Document;
 import nu.xom.Element;
 import nu.xom.Elements;
 import nu.xom.ParsingException;
+import nu.xom.Serializer;
 import exceptions.ToDoItemExistsException;
 
 /**
@@ -27,6 +32,82 @@ public class XMLFileToDoItemModel extends ToDoItemModel {
 		this.parseXML();
 	}
 	
+	
+	//Private help methods
+	private Date dateParser(String format, String value){
+		DateFormat df = new SimpleDateFormat(format);
+		Date date = null;
+		try {
+			date = df.parse(value);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return date;
+	}
+	
+	private String dateFormatter(String format, Date date){
+		DateFormat df = new SimpleDateFormat(format);
+		return df.format(date);
+	}
+	
+	private void addAttributesToXML(Document doc,ToDoItem item, Element todo){
+		Element title			= new Element("title");
+		Element desc 			= new Element("description");
+		Element dueDate			= new Element("duedate");
+		Element category		= new Element("category");
+		Element priority 		= new Element("priority");
+		Element creationDate 	= new Element("creationdate");
+		Element done		 	= new Element("done");
+		Element deleted		 	= new Element("deleted");
+		
+		//Add all attributes
+		todo.appendChild(title);
+		title.appendChild(item.getTitle());
+		
+		todo.appendChild(desc);
+		desc.appendChild(item.getDescription());
+		
+		todo.appendChild(dueDate);
+		String date1 = this.dateFormatter("yyy-MM-dd", item.getDueDate());
+		dueDate.appendChild(date1);
+		
+		todo.appendChild(category);
+		category.appendChild(item.getCategory().toString());
+		
+		todo.appendChild(priority);
+		priority.appendChild(Integer.toString(item.getPriority()));
+		
+		todo.appendChild(creationDate);
+		String date2 = this.dateFormatter("yyy-MM-dd'T'HH:mm", item.getDueDate());
+		creationDate.appendChild(date2);
+		
+		todo.appendChild(done);
+		String doneAttr = (true == item.isDone() ? "1" : "0");
+		done.appendChild(doneAttr);
+		
+		todo.appendChild(deleted);
+		String delAttr = (true == item.isDone() ? "1" : "0");
+		deleted.appendChild(delAttr);
+		
+		try {
+			FileOutputStream out = new FileOutputStream("data\\db.xml");
+			Serializer ser = new Serializer(out, "ISO-8859-1");
+	        ser.setIndent(4);
+	        ser.write(doc);
+	        out.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
 	/**
 	 * This method loads a xml-file that is used as a database and fills the data structure
 	 * of this class with the ToDoItems.
@@ -38,9 +119,8 @@ public class XMLFileToDoItemModel extends ToDoItemModel {
 			Document doc	= builder.build("data\\db.xml");
 			Element root 	= doc.getRootElement();
 			Elements todos	= root.getFirstChildElement("todoitems").getChildElements();
-			
-			ToDoItem task = null;
-			this.tasks = new ArrayList<ToDoItem>(todos.size());
+			ToDoItem task	= null;
+			this.tasks 		= new ArrayList<ToDoItem>(todos.size());
 			
 			for(int i = 0; i< todos.size(); i++){
 				Element title			 = todos.get(i).getFirstChildElement("title");
@@ -59,22 +139,15 @@ public class XMLFileToDoItemModel extends ToDoItemModel {
 				task.setTitle(title.getValue());
 				task.setDescription(desc.getValue());
 				
-				DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-				try {
-					task.setDueDate(df.parse(dueDate.getValue()));
-				} catch (ParseException e) {
-					e.printStackTrace();
-				}
 				
+				Date date1 = this.dateParser("yyy-MM-dd", dueDate.getValue());
+				task.setDueDate(date1);
 				task.setCategory(Category.valueOf(category.getValue()));
 				task.setPriority(Integer.parseInt(priority.getValue()));
 				
-				DateFormat df2 = new SimpleDateFormat("yyy-MM-dd'T'HH:mm");
-				try {
-					task.setCreationDate(df2.parse(creationDate.getValue()));
-				} catch (ParseException e) {
-					e.printStackTrace();
-				}
+				
+				Date date2 = this.dateParser("yyy-MM-dd'T'HH:mm", creationDate.getValue());
+				task.setCreationDate(date2);
 				boolean doneAttr = (1 == Integer.parseInt(done.getValue())) ? true : false;
 				task.setDone(doneAttr);
 				
@@ -93,6 +166,71 @@ public class XMLFileToDoItemModel extends ToDoItemModel {
 		}
 	}
 	
+	private void addItemXML(ToDoItem item){
+		try {
+			Builder builder = new Builder();			
+			Document doc = builder.build("data\\db.xml");
+			Element root 	= doc.getRootElement();
+			Element todos	= root.getFirstChildElement("todoitems");
+			Element todo 	= new Element("todoitem");
+			//Add new todoitem to todos
+			todos.appendChild(todo);
+			
+			this.addAttributesToXML(doc, item, todo);
+			
+		} catch (ParsingException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void updateItemXML(int index, ToDoItem item){
+		try {
+			Builder builder = new Builder();	
+			Document doc = builder.build("data\\db.xml");
+			Element root 	= doc.getRootElement();
+			Elements todos	= root.getFirstChildElement("todoitems").getChildElements();
+			todos.get(index).removeChildren();	
+			
+			this.addAttributesToXML(doc, item, todos.get(index));
+			
+		} catch (ParsingException | IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	private void editItemXML(ToDoItem item, String field, String value){
+		try {
+			Builder builder = new Builder();			
+			Document doc;
+			doc = builder.build("data\\db.xml");
+			Element root 	= doc.getRootElement();
+			Elements todos	= root.getFirstChildElement("todoitems")
+					.getChildElements();
+			
+			for(int i = 0; i< todos.size(); i++){
+				Element todo = todos.get(i);
+				if(todo.getFirstChildElement("title")
+						.getValue().equals(item.getTitle())){
+					
+					todo.getFirstChildElement(field).removeChildren();
+					todo.getFirstChildElement(field).appendChild(value);
+					break;
+				}
+			}
+			FileOutputStream out = new FileOutputStream("data\\db.xml");
+		    Serializer ser = new Serializer(out, "ISO-8859-1");
+		    ser.setIndent(4);
+		    ser.write(doc);
+		    out.close();
+			
+		} catch (ParsingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	@Override
 	public ArrayList<ToDoItem> getAllToDoItems() {
 		return this.tasks;
@@ -103,6 +241,7 @@ public class XMLFileToDoItemModel extends ToDoItemModel {
 		this.tasks.remove(index);
 		this.tasks.add(index, updatedItem);
 		// TODO save to file (here or in separate method; only this item or write complete list?)
+		this.updateItemXML(index, updatedItem);
 		setChanged();
 		notifyObservers();
 	}
@@ -116,6 +255,7 @@ public class XMLFileToDoItemModel extends ToDoItemModel {
 		else {
 			this.tasks.add(newItem);
 			// TODO save to file (here or in separate method; only this item or write complete list?)
+			this.addItemXML(newItem);
 			setChanged();
 			notifyObservers();
 		}
@@ -126,6 +266,7 @@ public class XMLFileToDoItemModel extends ToDoItemModel {
 	public void markToDoItemAsDone(ToDoItem item) {
 		this.tasks.get(this.getIndexOfToItem(item)).setDone(true);
 		// TODO save to file (here or in separate method; only this item or write complete list?)
+		this.editItemXML(item, "done", "1");
 		setChanged();
 		notifyObservers();
 	}
@@ -134,6 +275,7 @@ public class XMLFileToDoItemModel extends ToDoItemModel {
 	public void markToDoItemAsUndone(ToDoItem item) {
 		this.tasks.get(this.getIndexOfToItem(item)).setDone(false);
 		// TODO save to file (here or in separate method; only this item or write complete list?)
+		this.editItemXML(item, "done", "0");
 		setChanged();
 		notifyObservers();
 	}
@@ -142,6 +284,7 @@ public class XMLFileToDoItemModel extends ToDoItemModel {
 	public void deleteToDoItem(ToDoItem item) {
 		this.tasks.get(this.getIndexOfToItem(item)).setDeleted(true);
 		// TODO save to file (here or in separate method; only this item or write complete list?)
+		this.editItemXML(item, "deleted", "1");
 		setChanged();
 		notifyObservers();
 	}
@@ -150,6 +293,7 @@ public class XMLFileToDoItemModel extends ToDoItemModel {
 	public void restoreToDoItem(ToDoItem item) {
 		this.tasks.get(this.getIndexOfToItem(item)).setDeleted(false);
 		// TODO save to file (here or in separate method; only this item or write complete list?)
+		this.editItemXML(item, "deleted", "0");
 		setChanged();
 		notifyObservers();
 	}
