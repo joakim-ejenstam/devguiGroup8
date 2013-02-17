506bbdc759e85c1df27f456ee5b97ea4e9e608e3
Joakim Ejenstam <joakim.ejenstam@gmail.com>
2013-02-17 11:31:30 +0100

    Updating, some syntax error fixed!

--- src/controller/AddAction.java
+++ src/controller/AddAction.java
@@ -1,5 +1,8 @@
+import javax.swing.*;
+
+
 public class AddAction extends AbstractAction {
-	public AddAction(String text, ImageIcon icon
+	public AddAction(String text, ImageIcon icon,
 					 String desc, Integer mnemonic) {
 		super(text, icon);
 		putValue(SHORT_DESCRIPTION, desc);
