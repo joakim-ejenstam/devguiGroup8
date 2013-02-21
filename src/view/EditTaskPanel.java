fb88e13310ffca95a57ca867a0187a644bd85b38
Matteh <Matteh@123>
2013-02-21 21:20:17 +0100

    New design, wohooo. Cleaned up the whole class also.

--- src/view/EditTaskPanel.java
+++ src/view/EditTaskPanel.java
@@ -3,10 +3,20 @@ package view;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
-import java.util.Enumeration;
+import java.util.Date;
 
 //import javax.swing.BorderFactory;
-import javax.swing.*;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 import com.toedter.calendar.JDateChooser;
 import controller.ToDoController;
@@ -20,35 +30,30 @@ import model.ToDoItem;
 @SuppressWarnings("serial")
 public class EditTaskPanel extends JPanel {
     
-	ToDoItem item;
+	private ToDoController controller;
+	private ToDoItem item;
 	
     //Labels
-    private JLabel titleLabel            = new JLabel("Title:"); //lang.getText("ui.editview.label.title")
-    private JLabel dateLabel             = new JLabel("Date:"); //lang.getText("ui.editview.label.dueDate")
-    private JLabel timeLabel             = new JLabel("Time:"); //lang.getText("ui.editview.label.dueTime")
-    private JLabel descriptionLabel      = new JLabel("Description:"); //lang.getText("ui.editview.label.description")
-    private JLabel categoryLabel         = new JLabel("Category:"); //lang.getText("ui.editview.label.category")
-    //private JLabel reminderLabel         = new JLabel("Reminder:"); 
-    private JLabel priorityLabel         = new JLabel("Priority:"); //lang.getText("ui.editview.label.priority")
+    private JLabel titleLabel            = new JLabel("Title:");
+    private JLabel dateLabel             = new JLabel("Date:");
+    private JLabel timeLabel             = new JLabel("Time:");
+    private JLabel descriptionLabel      = new JLabel("Description:");
+    private JLabel categoryLabel         = new JLabel("Category:");
+    private JLabel priorityLabel         = new JLabel("Priority:");
 
     //TextFields
     private JTextField titleField       	= new JTextField(10);
-    //private JTextField dueDateField			= new JTextField(5);
-    //private JTextField dateToField      = new JTextField(5);
     private JTextField dueTimeField			= new JTextField(5);
-    //private JTextField endTimeField     = new JTextField(5);
     private JTextField categoryField    	= new JTextField(5);
-    //private JTextField reminderTimeField	= new JTextField(5);
-    //private JTextField remEndField      = new JTextField(5);
+
     
     //Text area
     private JTextArea descriptionArea    = new JTextArea(0,0);
     private JScrollPane	scrollArea 		 = new JScrollPane(descriptionArea);
     
-    /*Buttons
-    private JButton saveButton           = new JButton("Save");
-    private JButton cancelButton         = new JButton("Cancel");        
-    */
+    //Category JComboBox
+    private JComboBox categoryBox;
+    
     //Priority JRadioButtons
     private ButtonGroup priorityGroup    = new ButtonGroup();
     private JRadioButton lowPriority     = new JRadioButton("Low", true);
@@ -56,121 +61,101 @@ public class EditTaskPanel extends JPanel {
     private JRadioButton highPriority    = new JRadioButton("High", false);
 
     //Extra Panels
-    private JPanel datePanel			 = new JPanel();
-    private JPanel timePanel			 = new JPanel();
+    private JPanel topTitlePanel		 = new JPanel();
+    private JPanel topDatePanel			 = new JPanel();
     private JPanel categoryPanel		 = new JPanel();
+    private JPanel descCatPanel			 = new JPanel();
+    private JPanel topTimePanel			 = new JPanel();
     private JPanel bottomPanel           = new JPanel();
     private JPanel bottomLabelPanel		 = new JPanel();
     private JPanel priorityPanel	 	 = new JPanel();
-    //private JPanel reminderPanel		 = new JPanel();
     private JPanel subBottomPanel		 = new JPanel();
     private JPanel bottomCenterPanel	 = new JPanel();
     private JPanel topPanel              = new JPanel();
     private JPanel subTopPanel			 = new JPanel();
-    private JPanel topLabelPanel		 = new JPanel();
-    private JPanel topCenterPanel 	     = new JPanel();
     private JPanel buttonPanel			 = new JPanel();
     
     //JDates
-    //private JDateChooser reminderCal	= new JDateChooser();
     private JDateChooser dueDateCal		= new JDateChooser();
- //   private JDateChooser toDateCal		 = new JDateChooser();
     
     EditTaskPanel(ToDoController controller, ToDoItem item){
-    	
     	this.item = item;
+    	this.controller = controller;
+    	this.categoryBox = new JComboBox(); //TODO need reference to the category list from the controller
         controller.getOkAction().addPanel(this);
+        
+        
     	//Setup the different layoutManagers
         setLayout(new BorderLayout());
-        //setBorder(BorderFactory.createEmptyBorder());		TODO: fix padding instead
         topPanel.setLayout(new BorderLayout());
-        subTopPanel.setLayout(new BorderLayout());
-        topLabelPanel.setLayout(new BoxLayout(topLabelPanel, BoxLayout.PAGE_AXIS));
-        topCenterPanel.setLayout(new BoxLayout(topCenterPanel,BoxLayout.PAGE_AXIS));
-        datePanel.setLayout(new BoxLayout(datePanel, BoxLayout.LINE_AXIS));
-        timePanel.setLayout(new BoxLayout(timePanel, BoxLayout.LINE_AXIS));
+        subTopPanel.setLayout(new BoxLayout(subTopPanel, BoxLayout.PAGE_AXIS));
+        topTitlePanel.setLayout(new BoxLayout(topTitlePanel, BoxLayout.LINE_AXIS));
+        topDatePanel.setLayout(new BoxLayout(topDatePanel,BoxLayout.LINE_AXIS));
+        topTimePanel.setLayout(new BoxLayout(topTimePanel,BoxLayout.LINE_AXIS));
+        descCatPanel.setLayout(new BoxLayout(descCatPanel, BoxLayout.LINE_AXIS));
         categoryPanel.setLayout(new BoxLayout(categoryPanel, BoxLayout.LINE_AXIS));
         bottomPanel.setLayout(new BorderLayout());
         bottomLabelPanel.setLayout(new BoxLayout(bottomLabelPanel, BoxLayout.PAGE_AXIS));
         bottomCenterPanel.setLayout(new BoxLayout(bottomCenterPanel, BoxLayout.PAGE_AXIS));
         subBottomPanel.setLayout(new BorderLayout());
         priorityPanel.setLayout(new BoxLayout(priorityPanel, BoxLayout.LINE_AXIS));
-        //reminderPanel.setLayout(new BoxLayout(reminderPanel, BoxLayout.LINE_AXIS));
         buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.LINE_AXIS));
         
-        //Configuration for topLabelPanel
         
-        titleLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
-        topLabelPanel.add(Box.createRigidArea(new Dimension(0,10)));
-        topLabelPanel.add(titleLabel);
-        topLabelPanel.add(Box.createRigidArea(new Dimension(0,5)));
-        topLabelPanel.add(dateLabel);
-        topLabelPanel.add(Box.createRigidArea(new Dimension(0,5)));
-        topLabelPanel.add(timeLabel);
-        topLabelPanel.add(Box.createRigidArea(new Dimension(0,10)));
-        topLabelPanel.add(descriptionLabel);
+        //Configuration for topTitlePanel
+        topTitlePanel.setAlignmentX(Component.RIGHT_ALIGNMENT);
+        topTitlePanel.add(Box.createRigidArea(new Dimension(10,0)));
+        titleLabel.setPreferredSize(new Dimension(30,10));
+        topTitlePanel.add(titleLabel);
+        topTitlePanel.add(titleField);
         
         
         //Configuration for datePanel
-        datePanel.setAlignmentX(Component.RIGHT_ALIGNMENT);
-       // datePanel.add(Box.createRigidArea(new Dimension(5,0)));
-       // datePanel.add(Box.createRigidArea(new Dimension(5,0)));
-        datePanel.add(dueDateCal);
-        datePanel.add(Box.createRigidArea(new Dimension(5,0)));
-//        datePanel.add(new JLabel("To"));
-//        datePanel.add(Box.createRigidArea(new Dimension(5,0)));
-//        datePanel.add(Box.createRigidArea(new Dimension(5,0)));
-//        datePanel.add(toDateCal);
-//        datePanel.add(Box.createRigidArea(new Dimension(60,0)));	//TODO: do we need some right padding of the date field?
+        topDatePanel.setAlignmentX(Component.RIGHT_ALIGNMENT);
+        topDatePanel.add(Box.createRigidArea(new Dimension(10,0)));
+        dateLabel.setPreferredSize(new Dimension(30,10));
+        topDatePanel.add(dateLabel);
+        topDatePanel.add(dueDateCal);
         
         
-        //Configuration for timePanel
-        timePanel.setAlignmentX(Component.RIGHT_ALIGNMENT);
-        timePanel.add(dueTimeField);
-        timePanel.add(Box.createRigidArea(new Dimension(10,0)));
-//        timePanel.add(new JLabel("-"));
-//        timePanel.add(Box.createRigidArea(new Dimension(10,0)));	//TODO some padding needed here again?
-//        timePanel.add(endTimeField);
+        //Configuration for topTimePanel
+        topTimePanel.setAlignmentX(Component.RIGHT_ALIGNMENT);
+        topTimePanel.add(Box.createRigidArea(new Dimension(10,0)));
+        timeLabel.setPreferredSize(new Dimension(30,10));
+        topTimePanel.add(timeLabel);
+        topTimePanel.add(dueTimeField);
         
         
         //Configuration for category panel
         categoryPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
         categoryPanel.add(categoryLabel);
-        categoryPanel.add(Box.createRigidArea(new Dimension(10,0)));
-        categoryPanel.add(categoryField);
+        categoryPanel.add(Box.createRigidArea(new Dimension(4,0)));
+        categoryPanel.add(categoryBox);
+        categoryPanel.add(Box.createRigidArea(new Dimension(30,0)));
         
         
-        //Configuration for topCenterPanel
-        topCenterPanel.add(Box.createRigidArea(new Dimension(0,8)));
-        titleField.setAlignmentX(Component.RIGHT_ALIGNMENT);
-        //topLabelPanel.add(Box.createRigidArea(new Dimension(20,0)));
+        descCatPanel.add(Box.createRigidArea(new Dimension(10,0)));
+        descCatPanel.add(descriptionLabel);
+        descCatPanel.add(Box.createHorizontalGlue());
+        descCatPanel.add(categoryPanel);
 
-        topCenterPanel.add(titleField);
-        topCenterPanel.add(Box.createRigidArea(new Dimension(0,2)));
-        topCenterPanel.add(datePanel);
-        topCenterPanel.add(Box.createRigidArea(new Dimension(0,2)));
-        topCenterPanel.add(timePanel);
-        topCenterPanel.add(Box.createRigidArea(new Dimension(0,2)));
-        topCenterPanel.add(categoryPanel);
-        topCenterPanel.add(Box.createRigidArea(new Dimension(0,5)));
+        
+        //Configuration for the top panel
+        subTopPanel.add(Box.createRigidArea(new Dimension(0,10)));
+        subTopPanel.add(topTitlePanel);
+        subTopPanel.add(Box.createRigidArea(new Dimension(0,2)));
+        subTopPanel.add(topDatePanel);
+        subTopPanel.add(Box.createRigidArea(new Dimension(0,2)));
+        subTopPanel.add(topTimePanel);
+        subTopPanel.add(Box.createRigidArea(new Dimension(0,2)));
+        subTopPanel.add(descCatPanel);
+        subTopPanel.add(Box.createRigidArea(new Dimension(0,5)));
 
         
         //Configuration for bottomLabels
         bottomLabelPanel.add(Box.createRigidArea(new Dimension(0,15)));
-        //bottomLabelPanel.add(reminderLabel);
-        
+        bottomLabelPanel.add(Box.createRigidArea(new Dimension(20,0)));
         bottomLabelPanel.add(priorityLabel);
-        
-        
-        //Configuration for reminderPanel
-        //reminderPanel.add(Box.createRigidArea(new Dimension(5,0)));
-        //reminderPanel.add(reminderCal);
-        //reminderPanel.add(Box.createRigidArea(new Dimension(5,0)));
-        //reminderPanel.add(reminderTimeField);
-//        reminderPanel.add(Box.createRigidArea(new Dimension(5,0)));
-//        reminderPanel.add(new JLabel("-"));
-//        reminderPanel.add(Box.createRigidArea(new Dimension(5,0)));	//TODO some padding still needed?
-//        reminderPanel.add(remEndField);
         
         
         //Configuration for priorityPanel
@@ -192,22 +177,19 @@ public class EditTaskPanel extends JPanel {
         
         //Configuration for bottomCenterPanel
         bottomCenterPanel.add(Box.createRigidArea(new Dimension(0,10)));
-        //bottomCenterPanel.add(reminderPanel);
         bottomCenterPanel.add(priorityPanel);
         bottomCenterPanel.add(buttonPanel);
         
         
         //Add everything together in the topPanel and bottomPanel
-        subTopPanel.add(topLabelPanel,BorderLayout.LINE_START);
-        subTopPanel.add(topCenterPanel,BorderLayout.CENTER);
         topPanel.add(subTopPanel,BorderLayout.PAGE_START);
         topPanel.add(Box.createRigidArea(new Dimension(0,5)));
         descriptionArea.setLineWrap(true);
         topPanel.add(scrollArea, BorderLayout.CENTER);
         subBottomPanel.add(bottomLabelPanel, BorderLayout.LINE_START);
         subBottomPanel.add(bottomCenterPanel,BorderLayout.CENTER);
-        //subBottomPanel.add(buttonPanel, BorderLayout.PAGE_END);
-        bottomPanel.add(subBottomPanel, BorderLayout.PAGE_END);  /// CENTER????
+        bottomPanel.add(subBottomPanel, BorderLayout.PAGE_END);
+        
         ////////Configuration for the main panel, EditTaskPanel
         add(topPanel, BorderLayout.CENTER);
         add(bottomPanel,BorderLayout.PAGE_END);
@@ -225,29 +207,9 @@ public class EditTaskPanel extends JPanel {
     }
 
     public ToDoItem getTodoItem() {
-        item.setDescription(descriptionArea.getText());
-        item.setDueDate(dueDateCal.getDate());
-        item.setCategory(new Category(categoryField.getText()));
-        item.setPriority(getSelectedPriority(priorityGroup));
+        	item.setDescription(descriptionArea.getText());
+        	item.setPriority(1);
+        	item.setCategory(new Category(categoryField.getText()));
         return item;
-    }
-
-    public int getSelectedPriority(ButtonGroup buttonGroup) {
-        for (Enumeration<AbstractButton> buttons = buttonGroup.getElements(); buttons.hasMoreElements();) {
-            AbstractButton button = buttons.nextElement();
-
-            if (button.isSelected()) {
-                String text = button.getText();
-
-                if (text == "Low") {
-                    return 1;
-                } else if(text == "Medium") {
-                    return 2;
-                } else {
-                    return 3;
-                }
-            }
-        }
-        return 0;
     }
 }
