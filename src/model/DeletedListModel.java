e9c6c0ddc41d625c282c41bb876a72969981e5b1
emra <emma.rangert@gmail.com>
2013-03-05 22:18:29 +0100

    added javadoc

--- src/model/DeletedListModel.java
+++ src/model/DeletedListModel.java
@@ -11,22 +11,38 @@ public class DeletedListModel extends AbstractListModel {
 	private ToDoItemModel itemModel;
 	private List<ToDoItem> deleted;
 	
+	/**
+	 * The constructor to create a list to do item model
+	 * @param listModel the underlying model
+	 */
 	public DeletedListModel(ToDoItemModel listModel) {
 		this.itemModel = listModel;
-        this.deleted = new ArrayList<ToDoItem>(10);
+        this.deleted = new ArrayList<ToDoItem>();
 		getDeletedItems();
 	}
 	
+	/**
+	 * Returns the value at the specified index.
+	 * @param row index where to retrieve the value.
+	 * @return the value at the specified index.
+	 */
 	@Override
 	public Object getElementAt(int index) {
 		return this.deleted.get(index);
 	}
 	
+	/**
+	 * Method for calculating the size (length) of the list.
+	 * @return the number of to do items in the list.
+	 */
 	@Override
 	public int getSize() {
 		return deleted.size();
 	}
 	
+	/**
+	 * Method for retrieving the deleted to do items.
+	 */
 	public void getDeletedItems() {
 		this.deleted = this.itemModel.getDeletedToDoItems();
 	}
